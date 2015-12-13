0490416fc32a27f8c17ff9f30ee547128550e0b9
romandunets <roman.dunets.email@gmail.com>
2015-12-13 20:34:46 +0200

    Add route for listing users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
-var router = express.Router();
+var mongoose = require('mongoose');
 
-/* GET users listing. */
+var router = express.Router();
+var User = require('../models/user.js');
+
+/* GET /users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find(function (err, users) {
+    if (err) return next(err);
+    res.json(users);
+  });
 });
 
 module.exports = router;
