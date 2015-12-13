fe6da8012f08881e0aeadccf966cab17507f9d87
romandunets <roman.dunets.email@gmail.com>
2015-12-13 20:35:03 +0200

    Add route for creating new user

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,11 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res, next) {
+  User.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
 module.exports = router;
